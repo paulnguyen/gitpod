f93bba00d41696336701abd044d9e1ef68f1585d
paulnguyen <paul.nguyen@sjsu.edu>
2023-01-31 22:02:51 -0800

    cleanup

--- spring-gumball/spring-gumball/src/test/java/com/example/springgumball/SpringGumballApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.example.springgumball;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SpringGumballApplicationTests {
-	
-}

bfcefbc7e8eacb80b7f5a182804de4ce5af00025
paulnguyen <paul.nguyen@sjsu.edu>
2023-03-09 20:46:57 -0800

    Initial Start of Port for Node.js Cashiers

---
+++ starbucks-project/spring-cashier/src/main/java/com/example/springcashier/SpringCashierApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcashier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCashierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCashierApplication.class, args);
+    }
+
+}
---
+++ starbucks-project/spring-cashier/src/test/java/com/example/springcashier/SpringCashierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcashier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCashierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
